6aa8a5a5fe5206a4e95b0700368e8ddd5989f0ea
Harman Ark <ark.harman@gmail.com>
2021-08-20 18:53:19 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const accordionBtns = document.querySelectorAll(".accordion");
+
+accordionBtns.forEach((accordion) => {
+  accordion.onclick = function () {
+    
+    this.classList.toggle("is-open");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  };
+});
