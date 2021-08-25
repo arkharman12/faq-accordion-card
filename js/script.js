bda5fa77213652a025046a541069da14a3b35a6b
Harman Ark <ark.harman@gmail.com>
2021-08-25 15:58:08 -0400

    Final changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
-const accordionBtns = document.querySelectorAll(".accordion");
+let questions = document.getElementsByClassName('accordion');
 
-accordionBtns.forEach((accordion) => {
-  accordion.onclick = function () {
+let i;
+let numOpened = 0;
+
+for(i=0;i<questions.length; i++) {
     
-    this.classList.toggle("is-open");
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      //this is if the accordion is open
-      content.style.maxHeight = null;
-    } else {
-      //if the accordion is currently closed
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  };
-});
+    questions[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        let answer = this.nextElementSibling;
+        
+        if (answer.style.maxHeight) {
+            answer.style.maxHeight = null;
+            numOpened--;
+        } else {
+            if (numOpened > 0) {
+                document.querySelectorAll(".accordion-content").forEach(a=>a.style.maxHeight = null);
+                document.querySelectorAll(".accordion").forEach(a=>a.classList.remove('active'))
+                this.classList.add('active');
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+                numOpened++;
+            }
+        }
+        
+    });
+    
+}
